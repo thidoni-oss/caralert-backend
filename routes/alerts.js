b581833fe14fc277a1bf77a91835fea568fdd214
thidoni-oss <thi.doni@gmail.com>
2026-05-28 21:02:33 -0300

    Update alerts.js

--- routes/alerts.js
+++ routes/alerts.js
@@ -135,13 +135,13 @@ module.exports = (db, io, admin) => {
 
   router.post('/:id/sighting', async (req, res) => {
     try {
-      const { reporterId, lat, lng, chavePix } = req.body;
+      const { reporterId, lat, lng, chavePix, fotoUrl } = req.body;
       const alertId = req.params.id;
 
       await db.query(
-        `INSERT INTO sightings (alert_id, location, chave_pix_testemunha)
-         VALUES ($1, ST_SetSRID(ST_MakePoint($2,$3),4326), $4)`,
-        [alertId, lng, lat, chavePix || null]
+        `INSERT INTO sightings (alert_id, location, chave_pix_testemunha, foto_url)
+         VALUES ($1, ST_SetSRID(ST_MakePoint($2,$3),4326), $4, $5)`,
+        [alertId, lng, lat, chavePix || null, fotoUrl || null]
       );
 
       await db.query(
